58d598e4f1c86544a0eeaaa0c42aa3f8f38b372c
Abiorh001 <abiolaadedayo1993@gmail.com>
2026-05-29 09:11:48 +0100

    chore: prepare v0.0.8 release

--- src/omniclaw/__init__.py
+++ src/omniclaw/__init__.py
@@ -141,7 +141,7 @@ from omniclaw.protocols.nanopayments import (
 )
 from omniclaw.trust.gate import TrustGate
 
-__version__ = "0.0.6"
+__version__ = "0.0.8"
 __all__ = [
     # Main Client
     "OmniClaw",
